47a49724e7cb3260a542f8dbe67bbd11da4eaad3
Don <stalker.anonim@mail.ru>
2019-04-11 02:46:17 +0300

    Changed input

--- console.c
+++ console.c
@@ -2,31 +2,18 @@
 
 int read_line(void)
 {
-	int size = BUF_SIZE;
-	char *buf = malloc(size);
-	if (buf == NULL) {
+	char *str = NULL;
+	ssize_t buf = 0;
+	
+	if (getline(&str, &buf, stdin) != -1) {
 		perror("I/O error");
 		return -1;
 	}
 
-	int pos = 0;
-	char c;
-	while ((c = getchar()) != EOF && c != '\n') {
-		if (pos >= size) {
-			char *tmp = realloc(buf, size << 1);
-			if (tmp == NULL)
-				goto error;
-			size <<= 1;
-			buf = tmp;
-		}
-		buf[pos++] = c;
-	}
-
-	buf[pos] = 0;
 	return 0;
+}
 
-error:
-	free(buf);
-	perror("I/O error");
-	return -1;
+int split(char *str, char **tokens)
+{
+
 }
--- console.h
+++ console.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUF_SIZE 100
 
 int read_line(void);
+int split(char *str, char **tokens);
 
 #endif
