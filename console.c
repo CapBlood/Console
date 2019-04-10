df0544e873e6c5ce01a8abe0fc3a08af85dae99d
Don <stalker.anonim@mail.ru>
2019-04-11 02:35:39 +0300

    Input is done

--- console.c
+++ console.c
@@ -3,21 +3,26 @@
 int read_line(void)
 {
 	int size = BUF_SIZE;
-
 	char *buf = malloc(size);
 	if (buf == NULL) {
 		perror("I/O error");
 		return -1;
 	}
 
+	int pos = 0;
 	char c;
 	while ((c = getchar()) != EOF && c != '\n') {
-		if (ftell(stdin) >= size) {
+		if (pos >= size) {
+			char *tmp = realloc(buf, size << 1);
+			if (tmp == NULL)
+				goto error;
 			size <<= 1;
-			buf = realloc(buf, size);
+			buf = tmp;
 		}
+		buf[pos++] = c;
 	}
 
+	buf[pos] = 0;
 	return 0;
 
 error:
